55271fe2326dd3025e33dc77708eeed8bf6a0b8e
rakesh-softoboticcs <97211884+rakesh-softoboticcs@users.noreply.github.com>
2022-11-21 08:22:19 +0000

    logic method is remaining

---
+++ index.js
@@ -0,0 +1,95 @@
+const AWS = require("aws-sdk");
+AWS.config.update({
+    region:"ap-south-1"
+});
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+const dynamodbTableName = 'product-inventory';
+const healthPath = '/health';
+const productPath = '/product';
+const productsPath = '/products';
+
+
+exports.handler = async function(event){
+    console.log("Request event: ",event);
+    let response;
+    switch(true){
+        case event.httpMethod === "GET" && event.path === healthPath:
+            response = await buildResponse(200);
+            break;
+        
+        case event.httpMethod === "GET"  && event.path === productPath:
+            response = await getProduct(event.queryStringParameters.productId);
+            break;
+
+        case event.httpMethod === "GET"  && event.path === productsPath:
+            response = await getProducts();
+            break;
+
+        case event.httpMethod === "POST"  && event.path === productPath:
+            response = await saveProduct();
+            break;
+
+        case event.httpMethod === "PATCH" && event.path === productPath:
+            const requestBody = JSON.parse(event.body)
+            response = await modifyProduct(requestBody.productId,requestBody.updateKey,requestBody.updateValue);
+            break;
+        
+        case event.httpMethod === "DELETE" && event.path === productPath:
+            response = await deleteProduct(JSON.parse(event.body).productId);
+            break;
+        default:
+            response = buildResponse(404,"404 Not Found")
+    
+    }
+    return response;
+}
+
+async function getProduct(productId){
+     const params = {
+        TableName:dynamodbTableName,
+        Key:{
+            'productId':productId
+        }
+     }
+     return await dynamodb.get(params).promise().then((response)=>{
+        return buildResponse(200,response.Item);
+     },(error)=>{
+        console.error("Do your custome error handling here.I am just gonna log it",error)
+     })
+}
+
+async function getProducts(){
+    const params = {
+        TableName:dynamodbTableName
+    }
+    const allProducts = await scanDynamoRecords(params,[])
+    const body = {
+        products:allProducts
+    }
+    return buildResponse(200,body)
+}
+
+async function scanDynamoRecords(scanParams,itemArray){
+    try{
+        const dyanmoData = await dynamodb.scan(scanParams).promise();
+        itemArray = itemArray.concat(dynamoData.Items);
+        if(dynamoData.LastEvaluateKey){
+            scanParams.ExclusiveStartkey = dyanmoData.LastEvaluateKey;
+            return await scanDynamoRecords(scanParams,itemArray)
+        }
+        return itemArray;
+    }catch(error){
+        console.error("Do your custome error handling here.I am just gonna log it",error);
+    }
+}
+
+
+function buildResponse(statusCode,body){
+    return{
+        statusCode:statusCode,
+        headers:{
+            "Content-Type":"application-json"
+        },
+        body:JSON.stringify(body)
+    }
+}
